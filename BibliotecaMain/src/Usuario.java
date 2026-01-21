2064641469528da5fed56e3ae07eaac1635171d6
Emi <emiliofuentesoto@hotmail.com>
2026-01-21 19:02:25 +0100

    Clase usuario

---
+++ BibliotecaMain/src/Usuario.java
@@ -0,0 +1,40 @@
+public class Usuario {
+
+    private boolean admin;
+    private String nombreUsuario;
+    private String contrasena;
+    
+
+    //Constructores
+    public Usuario(){
+        this.admin = false;
+        this.nombreUsuario = "none";
+        this.contrasena = "1234";
+    }
+
+    public Usuario(boolean admin){
+        this();
+        this.admin = true;
+    }
+
+    //Getter y Setters
+
+    public String getNombreUsuario(){
+        return this.nombreUsuario;
+    }
+
+    public void setNombre(String nombre){
+        this.nombreUsuario = nombreUsuario;
+        if(nombre == ""){
+            nombre = "none";
+        }
+    }
+
+    public void setContrasena(String contrasena){
+        this.contrasena = contrasena;
+    }
+
+    //Métodos
+
+    
+}
