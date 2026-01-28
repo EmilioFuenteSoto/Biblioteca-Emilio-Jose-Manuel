d4e63f1cdca4203ee7ffb0aa1344c078d6d5200b
josemanuel <jmcanog03@iesalbarregas.es>
2026-01-28 19:29:11 +0100

    menu_diseñado

--- BibliotecaMain/src/App.java
+++ BibliotecaMain/src/App.java
@@ -17,7 +17,11 @@ public class App {
         Usuario usuario1 = new Usuario("emi", "emi", false);
         biblioteca.anadirUsuarios(usuario1);
 
-        
+        Libros libro1 = new Libros("un monstruo viene verme", "stphen king", "miedo");
+
+        biblioteca.anadirLibros(libro1);
+
+
         comUsuarios.login();
 
     }
